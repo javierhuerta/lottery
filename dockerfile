15aaf0d74764bfe48ab65c1ae13610af2ba251ca
Javier Huerta <javierhuerta@unach.cl>
2023-05-02 10:35:31 -0400

    fixes

--- dockerfile
+++ dockerfile
@@ -1,15 +1,15 @@
-FROM python:3.7
-
+FROM python:3.10.4
 
 ENV PYTHONUNBUFFERED 1
-
 
 RUN mkdir /code
 WORKDIR /code
 
-COPY requirements.txt /code/
+COPY requirements.txt .
 RUN pip install -r requirements.txt
 
-COPY . /code/
+COPY . .
 
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+
+EXPOSE 8000
--- lottery/settings.py
+++ lottery/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'lottery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'loteria',
+        'USER': 'loteria',
+        'PASSWORD': 'loteria12345678',
+        'HOST': 'host.docker.internal',
+        'PORT': '3306',
     }
 }
 
